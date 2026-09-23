c531dce66ac54ee4b87d4ae5db50312443ae77bc
Javed Mujawar <mujawar.javadu@gmail.com>
2026-09-23 16:58:30 +0530

    calculate v1

--- simple-agent-3.py
+++ simple-agent-3.py
@@ -4,26 +4,47 @@ from langchain.tools import tool
 
 
 @tool
-def calculate(a, b):
-    """this is tool/ function which take two number and return sum/addtion of two numbers"""
-    return a + b
+def find_all_arguments(text: str) -> list[int]:
+    """Extract all numbers from the user's mathematical question."""
+    return [int(number) for number in re.findall(r"\d+", text)]
+
+@tool
+def find_opration(text: str) -> str:
+    """Extract all numbers from the user's mathematical question."""
+    return text
+
+
+@tool
+def calculate(arr: list[int], method: str) -> int:
+    """Calculate numbers using the requested mathematical operation."""
+
+    if method == "add":
+        return sum(arr)
+
+    if method == "multiply":
+        result = 1
+        for number in arr:
+            result *= number
+        return result
+
+    return 0
 
 
 model = ChatOllama(model="qwen3:1.7b")
 
 agent = create_agent(
     model,
-    tools=[calculate],
-    system_prompt="you are a agent, that help to calculate, which provided by user",
+    tools=[find_all_arguments, find_opration, calculate],
+    system_prompt="you are a agent, that help to calculate, which provided by user. As final result you need to send only result in number full message is not needed. final message should result: result",
 )
 
 
 def runAgent(usrPrompt: str):
-    """ this is main agent function where we are putting all steps and logic"""
+    """this is main agent function where we are putting all steps and logic"""
     result = agent.invoke({"messages": [{"role": "user", "content": usrPrompt}]})
 
-    print(result)
+    print(result["messages"][-1].content)
 
 
 if __name__ == "__main__":
-    runAgent("What is sum of 2 and 5")
+    runAgent("calculate multiplication of 2 3 4 and 8")
